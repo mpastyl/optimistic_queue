34b8dc78335d49431ef1544ac3f1fdf391ef020a
Mpampis Stylianopoulos <mpastyl12@gmail.com>
2014-05-19 14:29:05 +0300

    all working (ABA not handled)

--- optimisti_queue.c
+++ optimisti_queue.c
@@ -16,11 +16,13 @@ struct queue_t{
 };
 
 
+int DUMMY_VAL = 101;
+
 void initialize(struct queue_t * Q){
     struct node_t * node = (struct node_t * ) malloc(sizeof(struct node_t));
     node->next =  NULL;
     node->prev =  NULL;
-    node->value =  101;
+    node->value =  DUMMY_VAL;
     Q->Head  =  node;
     Q->Tail =  node;
 }
@@ -42,20 +44,89 @@ void enqueue(struct queue_t * Q,int val){
 
 }
 
+int dequeue(struct queue_t * Q,int * p_val){
+    
+    struct node_t * tail;
+    struct node_t * head;
+    struct node_t * firstNodePrev;
+    int val;
+    while (1){
 
+        head = Q->Head;
+        tail = Q->Tail;
+        firstNodePrev = head->prev;
+        val =  head-> value;
+        if (head == Q->Head){
+            if (val!=DUMMY_VAL){
+                if (tail!=head){
+                    //TODO: add tag check and fix list
+                }
+
+                else{
+                    struct node_t * nd_dummy = (struct node_t * ) malloc(sizeof(struct node_t));
+                    nd_dummy->value =  DUMMY_VAL;
+                    nd_dummy->next = tail;
+                    if (__sync_bool_compare_and_swap(&Q->Tail,tail,nd_dummy)){
+                        head->prev = nd_dummy;
+                    }
+                    else free(nd_dummy);
+                    continue;
+                }
+                if(__sync_bool_compare_and_swap(&Q->Head,head,firstNodePrev)){
+                    free(head);
+                    *p_val = val;
+                    return 1;
+                }
+            }
+            else{
+                if (tail == head) return 0;
+                else {
+                    //TODO: add tag check and call fixlist
+                    int temp = __sync_bool_compare_and_swap(&Q->Head,head,firstNodePrev);
+                }
+            }
+        }
+    }
+
+
+            
+}
+
+void printqueue(struct queue_t * Q){
+
+    struct node_t * curr ;
+    struct node_t * next ;
+    struct node_t * prev;
+    curr = Q->Head;
+    next = Q->Head->next;
+    prev = Q->Head->prev;
+    while (curr != Q->Tail){
+        printf("%d ",curr->value);
+        curr = prev;
+        prev = curr ->prev;
+    }
+    printf("%d ",curr->value);
+    printf("\n");
+
+}
 
 
 int main(int argc,char * argv[]){
 
-
+    int res,val=0;
     struct queue_t * Q = (struct queue_t *) malloc(sizeof(struct queue_t));
     initialize(Q);
 
     enqueue(Q,5);
     enqueue(Q,8);
     enqueue(Q,12);
-    printf(" %d \n",Q->Head->value);
-    printf(" %d \n",Q->Head->prev->value);
-    printf(" %d \n",Q->Head->prev->prev->value);
+    res=dequeue(Q,&val);
+    res=dequeue(Q,&val);
+    //if (res) printf("  dequeued %d \n",val);
+    enqueue(Q,7);
+    //printf(" %d \n",Q->Head->value);
+    //printf(" %d \n",Q->Head->prev->value);
+    //printf(" %d \n",Q->Head->prev->prev->value);
+    printqueue(Q);
     return 1;
 }
