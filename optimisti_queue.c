699551cab57bef5d24190c4d7c68f20e361b1fa8
Mpampis Stylianopoulos <mpastyl12@gmail.com>
2014-05-22 16:16:25 +0300

    fixed wrong casting to __int128

--- optimisti_queue.c
+++ optimisti_queue.c
@@ -4,7 +4,7 @@
 
 
 struct pointer_t{
-    unsigned long long both;
+    __int128 both;
 };
 
 struct node_t{
@@ -22,35 +22,35 @@ struct queue_t{
 
 int DUMMY_VAL = 101;//TODO: change this
 
-unsigned long long get_count(unsigned long long a){
+__int128 get_count(__int128 a){
 
-    unsigned long long b = a >>48;
+    __int128 b = a >>64;
     return b;
 }
 
-unsigned long long get_pointer(unsigned long long a){
-    unsigned long long b = a << 16;
-    b= b >>16;
+__int128 get_pointer(__int128 a){
+    __int128 b = a << 64;
+    b= b >>64;
     return b;
 }
 
-unsigned long long set_count(unsigned long long  a, unsigned long long count){
-    unsigned long long count_temp =  count << 48;
-    unsigned long long b = get_pointer(a);
+__int128 set_count(__int128  a, __int128 count){
+    __int128 count_temp =  count << 64;
+    __int128 b = get_pointer(a);
     b = b | count_temp;
     return b;
 }
 
-unsigned long long set_pointer(unsigned long long a, unsigned long long ptr){
-    unsigned long long b = 0;
-    unsigned long long c = get_count(a);
+__int128 set_pointer(__int128 a, __int128 ptr){
+    __int128 b = 0;
+    __int128 c = get_count(a);
     b = set_count(b,c);
     ptr = get_pointer(ptr);
     b= b | ptr;
     return b;
 }
 
-unsigned long long set_both(unsigned long long a, unsigned long long ptr, unsigned long long count){
+__int128 set_both(__int128 a, __int128 ptr, __int128 count){
     a=set_pointer(a,ptr);
     a=set_count(a,count);
     return a;
@@ -71,12 +71,13 @@ void enqueue(struct queue_t * Q,int val){
     
     struct pointer_t  tail;
     struct node_t * nd = (struct node_t *) malloc(sizeof(struct node_t));
+    //printf("%p \n",&nd);
     nd->value = val;
     while (1){
         tail = Q->Tail;
-        nd->next.both = set_both(nd->next.both,tail.both,get_count(tail.both)+1);
-        struct node_t * new_to_set = set_both(new_to_set,nd,get_count(tail.both)+1);
-        //unsigned long long temp =  Q->Tail.both;
+        nd->next.both = set_both(nd->next.both,tail.both,get_count(tail.both)+(__int128)1);
+        __int128 new_to_set = set_both(new_to_set,nd,get_count(tail.both)+(__int128)1);
+        //__int128 temp =  Q->Tail.both;
         if (__sync_bool_compare_and_swap(&(Q->Tail.both),tail.both,new_to_set)){
             struct pointer_t prev =  ((struct node_t *)get_pointer(tail.both))->prev;
             prev.both = set_both(prev.both,nd,get_count(tail.both));
@@ -100,10 +101,10 @@ void fixList(struct queue_t * Q,struct pointer_t tail,struct pointer_t head){
             return;
         }
         nextNodePrev = ((struct node_t *)get_pointer(currNodeNext.both))->prev;
-        if((get_pointer(nextNodePrev.both)!=get_pointer(currNode.both))||(get_count(nextNodePrev.both)!=(get_count(currNode.both) -1))){
-            ((struct node_t *)get_pointer(currNodeNext.both))->prev.both = set_both(((struct node_t *)get_pointer(currNodeNext.both))->prev.both,currNode.both,get_count(currNode.both)-1);
+        if((get_pointer(nextNodePrev.both)!=get_pointer(currNode.both))||(get_count(nextNodePrev.both)!=(get_count(currNode.both) -(__int128)1))){
+            ((struct node_t *)get_pointer(currNodeNext.both))->prev.both = set_both(((struct node_t *)get_pointer(currNodeNext.both))->prev.both,currNode.both,get_count(currNode.both)-(__int128)1);
         }
-        currNode.both = set_both(currNode.both,currNodeNext.both,get_count(currNode.both) -1);
+        currNode.both = set_both(currNode.both,currNodeNext.both,get_count(currNode.both) -(__int128)1);
     }
 }
 
@@ -113,7 +114,7 @@ int dequeue(struct queue_t * Q,int * p_val){
     struct pointer_t  head;
     struct pointer_t  firstNodePrev;
     int val;
-    struct node_t * new_to_set;
+    __int128 new_to_set;
     while (1){
 
         head = Q->Head;
@@ -132,15 +133,15 @@ int dequeue(struct queue_t * Q,int * p_val){
                 else{
                     struct node_t * nd_dummy = (struct node_t * ) malloc(sizeof(struct node_t));
                     nd_dummy->value =  DUMMY_VAL;
-                    nd_dummy->next.both = set_both(nd_dummy->next.both,tail.both,get_count(tail.both)+1);
-                    new_to_set = set_both(new_to_set,nd_dummy,get_count(tail.both)+1);
+                    nd_dummy->next.both = set_both(nd_dummy->next.both,tail.both,get_count(tail.both)+(__int128)1);
+                    new_to_set = set_both(new_to_set,nd_dummy,get_count(tail.both)+(__int128)1);
                     if (__sync_bool_compare_and_swap(&(Q->Tail.both),tail.both,new_to_set)){
                         ((struct node_t *)get_pointer(head.both))->prev.both =set_both(((struct node_t *)get_pointer(head.both))->prev.both,nd_dummy,get_count(tail.both));
                     }
                     else free(nd_dummy);
                     continue;
                 }
-                new_to_set =  set_both(new_to_set,firstNodePrev.both,get_count(head.both)+1);
+                new_to_set =  set_both(new_to_set,firstNodePrev.both,get_count(head.both)+(__int128)1);
                 if(__sync_bool_compare_and_swap(&(Q->Head.both),head.both,new_to_set)){
                     free((struct node_t *)get_pointer(head.both));
                     *p_val = val;
@@ -155,7 +156,7 @@ int dequeue(struct queue_t * Q,int * p_val){
                         fixList(Q,tail,head);
                         continue;
                     }
-                    new_to_set = set_both(new_to_set,firstNodePrev.both,get_count(head.both)+1);
+                    new_to_set = set_both(new_to_set,firstNodePrev.both,get_count(head.both)+(__int128)1);
                     int temp = __sync_bool_compare_and_swap(&(Q->Head.both),head.both,new_to_set);
                 }
             }
@@ -238,6 +239,6 @@ int main(int argc,char * argv[]){
     double time_res = timer_report_sec(timer);
     printf("num_threasd %d  enq-deqs total %d \n",num_threads,count);
     printf("Total time  %lf \n",time_res);
-
+    printf("tail tag %d\n",get_count(Q->Tail.both));
     return 1;
 }
